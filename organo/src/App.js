d7c1c793dd8d2c759a3034f6f9232c41a9177c93
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-04-22 21:51:57 -0300

    Implementação component banner

---
+++ organo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './componentes/Banner/Banner';
+
+function App() {
+  return (
+    <>
+      <Banner/>
+    </>
+  );
+}
+
+export default App;

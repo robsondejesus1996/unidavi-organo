32442e29cdfd78b6ac7f0541084cebde74face03
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-06-04 16:38:30 -0300

    feat: list of team

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,7 +1,21 @@
 import CaompoTexto from "../CompoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+
+
+  // aqui pode ser todos os departamentos que vai ter o filtro para trazer para os departamentos
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +23,7 @@ const Formulario = () => {
         <CaompoTexto label="Nome" placeholder="Digite o seu nome" />
         <CaompoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CaompoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times}/>
       </form>
     </section>
   );

54f34b0725d09adad06f6f33ed4c7398abb088a4
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-06-05 14:30:14 -0300

    feat: create button post

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,20 +1,19 @@
+import Botao from "../Botao";
 import CaompoTexto from "../CompoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
-
-
   // aqui pode ser todos os departamentos que vai ter o filtro para trazer para os departamentos
   const times = [
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops',
-    'UX e Design',
-    'Mobile',
-    'Inovação e Gestão'
-  ]
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
   return (
     <section className="formulario">
@@ -23,7 +22,9 @@ const Formulario = () => {
         <CaompoTexto label="Nome" placeholder="Digite o seu nome" />
         <CaompoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CaompoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times}/>
+        <ListaSuspensa label="Time" itens={times} />
+
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );

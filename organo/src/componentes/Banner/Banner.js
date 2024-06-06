e94d067b0695f49ccbc3a50a13c280d5ff66ef54
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-06-06 19:47:46 -0300

    feat: elevate list teams

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -4,10 +4,12 @@ function Banner() {
   return (
     <header className="banner">
       <img
-        src="/imagens/banner.png"
+        src="https://siteunidavi.s3-sa-east-1.amazonaws.com/2021/5/topo-mentor-55anos.jpg"
         alt="Banner principal da pagina do Organo"
       />
     </header>
+
+    //https://siteunidavi.s3-sa-east-1.amazonaws.com/2021/5/topo-mentor-55anos.jpg   /imagens/banner.png
   );
 }
 

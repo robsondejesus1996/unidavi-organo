5658306b55c8b0d2e36f7b4c8029c88a6346f4e4
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-06-07 19:52:22 -0300

    fix: adjusts warnings console and map colaboradores

--- organo/src/App.js
+++ organo/src/App.js
@@ -51,7 +51,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    //console.log(colaborador);
     setColaboradores([...colaboradores, colaborador]);
   };
 
